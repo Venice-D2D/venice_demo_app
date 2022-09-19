09a06096a84cfda921d9292fd443254ba2456cad
Remy Raes <raes.remy@gmail.com>
2022-09-19 22:23:37 +0200

    docs: update app title

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'File exchange example app'),
     );
   }
 }

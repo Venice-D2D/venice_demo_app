36dbfd744e61593577fe69a78b36e27e185cd6f2
Remy Raes <raes.remy@gmail.com>
2022-09-20 17:30:43 +0200

    refactor: export sender view to dedicated component

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,5 @@
-import 'dart:io';
-
-import 'package:channel_multiplexed_scheduler/channels/abstractions/bootstrap_channel.dart';
-import 'package:channel_multiplexed_scheduler/scheduler/scheduler.dart';
-import 'package:file_exchange_example_app/channelTypes/bootstrap_channel_type.dart';
-import 'package:file_exchange_example_app/channelTypes/data_channel_type.dart';
-import 'package:file_exchange_example_app/scheduler_implementation.dart';
-import 'package:file_picker/file_picker.dart';
+import 'package:file_exchange_example_app/views/senderView.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:qr_code_bootstrap_channel/qr_code_bootstrap_channel.dart';
-import 'package:wifi_data_channel/wifi_data_channel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,26 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  BootstrapChannelType _bootstrapChannelType = BootstrapChannelType.qrCode;
-  final List<DataChannelType> _dataChannelTypes = [];
-  File? _file;
-
-  void _setBootstrapChannelType(BootstrapChannelType type) {
-    setState(() {
-      _bootstrapChannelType = type;
-    });
-  }
-  
-  void _toggleDataChannelType(DataChannelType type) {
-    setState(() {
-      if (_dataChannelTypes.contains(type)) {
-        _dataChannelTypes.remove(type);
-      } else {
-        _dataChannelTypes.add(type);
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -77,121 +47,13 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           body: TabBarView(
             children: [
-              _buildSenderView(),
-              _buildReceiverView()
+              const SenderView(),
+              Column()
             ],
           ),
         ),
       ),
 
     );
-  }
-
-  Widget _buildSenderView() {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(top: 50),
-            child: ElevatedButton(
-              onPressed: () async {
-                FilePickerResult? result = await FilePicker.platform.pickFiles();
-                if (result != null) {
-                  setState(() {
-                    _file = File(result.files.single.path!);
-                  });
-                } else {
-                  debugPrint("User selected no file.");
-                }
-              },
-              child: Text(
-                _file != null
-                  ? _file!.uri.pathSegments.last
-                  : "Select file to send"
-              )
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-            child: const Divider(),
-          ),
-          Container(
-            margin: const EdgeInsets.all(20),
-            child: const Text(
-              'Select bootstrap channel:',
-            ),
-          ),
-          ListTile(
-            title: const Text('QR code'),
-            onTap: () => _setBootstrapChannelType(BootstrapChannelType.qrCode),
-            trailing: Checkbox(
-                value: _bootstrapChannelType == BootstrapChannelType.qrCode,
-                onChanged: (v) => _setBootstrapChannelType(BootstrapChannelType.qrCode)),
-          ),
-          Container(
-            margin: const EdgeInsets.all(20),
-            child: const Text(
-              'Select data channels (at least one):',
-            ),
-          ),
-          ListTile(
-            title: const Text('Wi-Fi'),
-            onTap: () => _toggleDataChannelType(DataChannelType.wifi),
-            trailing: Checkbox(
-              value: _dataChannelTypes.contains(DataChannelType.wifi),
-              onChanged: (v) => _toggleDataChannelType(DataChannelType.wifi)
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: ElevatedButton(
-        onPressed: _canSendFile() ? () => _startSendingFile(context) : null,
-        style: ButtonStyle(
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            const RoundedRectangleBorder( borderRadius: BorderRadius.zero )
-          )
-        ),
-        child: Container(
-          margin: const EdgeInsets.all(20),
-          child: const Text("Send file"),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildReceiverView() {
-    return Column();
-  }
-
-  bool _canSendFile() {
-    return _file != null && _dataChannelTypes.isNotEmpty;
-  }
-
-  Future<void> _startSendingFile(BuildContext context) async {
-    if (_file == null) {
-      Fluttertoast.showToast(
-          msg: "Select a file before starting file sending."
-      );
-      return;
-    }
-
-    Fluttertoast.showToast(
-      msg: "Starting to send ${_file!.uri.pathSegments.last}..."
-    );
-
-    BootstrapChannel bootstrapChannel = QrCodeBootstrapChannel(context);
-    Scheduler scheduler = SchedulerImplementation(bootstrapChannel);
-
-    // add data channels
-    for (var type in _dataChannelTypes) {
-      switch(type) {
-        case DataChannelType.wifi:
-          scheduler.useChannel( WifiDataChannel("wifi_data_channel") );
-          break;
-      }
-    }
-
-    await scheduler.sendFile(_file!, 100000);
-    Fluttertoast.showToast( msg: "File successfully sent!" );
   }
 }

c5ed65146beeee75fa2eadfa84d95d756d92f24a
Remy Raes <contact@remyraes.com>
2023-11-24 11:40:18 +0100

    refactor: implement new venice_core api

--- lib/scheduler_implementation.dart
+++ lib/scheduler_implementation.dart
@@ -1,18 +1,19 @@
 import 'package:async/src/cancelable_operation.dart';
 import 'package:venice_core/channels/abstractions/data_channel.dart';
-import 'package:venice_core/file/file_chunk.dart';
 import 'package:delta_scheduler/scheduler/scheduler.dart';
+import 'package:venice_core/network/message.dart';
 
 class SchedulerImplementation extends Scheduler {
   SchedulerImplementation(super.bootstrapChannel);
 
   @override
-  Future<void> sendChunks(List<FileChunk> chunks, List<DataChannel> channels, Map<int, CancelableOperation> resubmissionTimers) async {
-    while (chunks.isNotEmpty || resubmissionTimers.isNotEmpty) {
-      if (chunks.isEmpty) {
+  Future<void> sendMessages(List<VeniceMessage> messages, List<DataChannel> channels, Map<int, CancelableOperation> resubmissionTimers) async {
+    while (messages.isNotEmpty || resubmissionTimers.isNotEmpty) {
+      if (messages.isEmpty) {
         await Future.delayed(const Duration(milliseconds: 200));
       } else {
-        sendChunk(chunks.removeAt(0), channels[0]);
+        // TODO remove await
+        await sendMessage(messages.removeAt(0), channels[0]);
       }
     }
   }

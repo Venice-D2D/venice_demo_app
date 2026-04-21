e6b657111d69a1277953cce8efce724e228a6848
Rémy Raes <contact@remyraes.com>
2026-04-21 15:48:14 +0200

    feat: integrate current_wifi_data_channel to app

--- lib/channelTypes/data_channel_type.dart
+++ lib/channelTypes/data_channel_type.dart
@@ -1,22 +1,24 @@
+import 'package:current_wifi_data_channel/current_wifi_data_channel.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:venice_core/channels/abstractions/data_channel.dart';
 import 'package:wifi_data_channel/wifi_data_channel.dart';
 
-enum DataChannelType {
-  wifi
-}
+enum DataChannelType { wifi, currentWifi }
 
 extension DataChannelTypeUtils on DataChannelType {
   String get label {
-    switch(this) {
+    switch (this) {
       case DataChannelType.wifi:
-        return 'Wi-Fi';
+        return 'Wi-Fi (hotspot)';
+      case DataChannelType.currentWifi:
+        return 'Wi-Fi (shared network)';
     }
   }
 
   List<Permission> get neededPermissions {
-    switch(this) {
+    switch (this) {
       case DataChannelType.wifi:
+      case DataChannelType.currentWifi:
         return [
           Permission.locationWhenInUse,
           // Prompt user for nearby devices detection permission (on Android SDK > 32)
@@ -27,9 +29,11 @@ extension DataChannelTypeUtils on DataChannelType {
 
   /// Returns a data channel that can be used in data exchanges.
   DataChannel get dataChannel {
-    switch(this) {
+    switch (this) {
       case DataChannelType.wifi:
         return WifiDataChannel("wifi_data_channel");
+      case DataChannelType.currentWifi:
+        return CurrentWifiDataChannel('current_wifi_data_channel');
     }
   }
 }
